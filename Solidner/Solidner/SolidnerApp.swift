4b0f42d00515d4b2ca2553e3af0722c1635bf61f
earlysummer0303 <jw9914@naver.com>
2023-11-06 21:02:11 +0900

    add: 프로젝트 폴더 구조 세팅 완료

---
+++ Solidner/Solidner/SolidnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolidnerApp.swift
+//  Solidner
+//
+//  Created by 황지우2 on 2023/11/06.
+//
+
+import SwiftUI
+
+@main
+struct SolidnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
