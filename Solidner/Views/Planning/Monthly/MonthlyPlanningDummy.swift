c3006388280524072939382f346112985ef24e9b
220v-K <jwlee010222@gmail.com>
2023-11-09 00:28:37 +0900

    [Add]: Add View Foldering

---
+++ Solidner/Views/Planning/Monthly/MonthlyPlanningDummy.swift
@@ -0,0 +1,8 @@
+//
+//  MonthlyPlanningDummy.swift
+//  Solidner
+//
+//  Created by 이재원 on 2023/11/09.
+//
+
+import Foundation
---
+++ Solidner/Views/Planning/Weekly/WeeklyPlanningDummy.swift
@@ -0,0 +1,8 @@
+//
+//  WeeklyPlanningDummy.swift
+//  Solidner
+//
+//  Created by 이재원 on 2023/11/09.
+//
+
+import Foundation
