11912da07c787976956e2c1e16f4d587be5e9fd5
sei <vanism7557@gmail.com>
2023-11-10 02:58:16 +0900

    [Chore/#4]: View를 그리기 위해 임시적인 데이터 구조를 만들었습니다. --- 1. 임시 데이터 구조   - MealPlan   - Meal   - MealTime   - Ingredient 2. 해당 커밋에서 Preview 코드가 2개 공존하는데.. 모든 코드 작성 후 분할 커밋 중이라서 :)

--- Solidner/Views/Planning/Weekly/WeeklyPlanningView.swift
+++ Solidner/Views/Planning/Weekly/WeeklyPlanningView.swift
@@ -24,3 +24,57 @@ struct WeeklyPlanningView: View {
 #Preview {
     WeeklyPlanningView()
 }
+extension WeeklyPlanningView {
+    // MARK: - Mock Meals
+
+    struct MealPlan: Identifiable {
+        var id: UUID = UUID()
+        var startDate: Date
+        var endDate: Date
+        var meals: [Meal]
+        var solidFoodDays: Int
+
+        struct Meal: Identifiable, Hashable {
+            var id: UUID = UUID()
+
+            var ingredients: [Ingredient]
+            var time: MealTime
+
+            var ingredientsString: String {
+                ingredients.map { $0.description }.joined(separator: ", ")
+            }
+
+        }
+
+        enum MealTime {
+            case 아침, 점심, 저녁, 간식1, 간식2
+
+            var icon: String {
+                switch self {
+                case .아침:
+                    return "sun.max"
+                case .점심:
+                    return "sun.horizon"
+                case .저녁:
+                    return "moon"
+                case .간식1:
+                    return "1.circle"
+                case .간식2:
+                    return "2.circle"
+                }
+            }
+        }
+
+        struct Ingredient: CustomStringConvertible, Hashable {
+            var description: String { name }
+
+            var isNew: Bool
+            var name: String
+        }
+    }
+}
+
+
+//#Preview {
+//    WeeklyPlanningView()
+//}
