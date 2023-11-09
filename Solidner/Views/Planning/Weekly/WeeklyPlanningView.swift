1938d3cb0ec8b3dd118dffbc55066e50fc82c11f
sei <vanism7557@gmail.com>
2023-11-10 03:07:52 +0900

    [Chore/#4]: MockData 추가

--- Solidner/Views/Planning/Weekly/WeeklyPlanningView.swift
+++ Solidner/Views/Planning/Weekly/WeeklyPlanningView.swift
@@ -96,6 +96,55 @@ extension Date {
 extension WeeklyPlanningView {
     // MARK: - Mock Meals
 
+    var mockMealsZero: [MealPlan] { [] }
+    var mockMealsOne: [MealPlan] {
+        let meal14: [MealPlan.Meal] = [
+            .init(ingredients:
+                    [.init(isNew: true, name: "소고기"), .init(isNew: false, name: "쌀")],
+                  time: .아침)
+        ]
+        let meal17: [MealPlan.Meal] = [
+            .init(ingredients:
+                    [.init(isNew: true, name: "밀가루"), .init(isNew: false, name: "쌀"), .init(isNew: false, name: "소고기"), .init(isNew: false, name: "양배추"), .init(isNew: false, name: "단호박"), .init(isNew: false, name: "사과")],
+                  time: .점심)
+        ]
+        let meal20: [MealPlan.Meal] = [
+            .init(ingredients:
+                    [.init(isNew: false, name: "쌀"), .init(isNew: false, name: "소고기"), .init(isNew: false, name: "브로콜리"), .init(isNew: false, name: "사과")],
+                  time: .점심),
+            .init(ingredients:
+                    [.init(isNew: true, name: "아스파라거스"), .init(isNew: false, name: "쌀"), .init(isNew: false, name: "소고기"),.init(isNew: false, name: "단호박"), ],
+                  time: .저녁),
+        ]
+        let meal23: [MealPlan.Meal] = [
+            .init(ingredients:
+                    [.init(isNew: false, name: "쌀"), .init(isNew: false, name: "소고기"), .init(isNew: false, name: "사과"), .init(isNew: false, name: "브로콜리")],
+                  time: .아침),
+            .init(ingredients:
+                    [.init(isNew: false, name: "쌀"), .init(isNew: false, name: "소고기"), .init(isNew: false, name: "단호박"), .init(isNew: false, name: "아스파라거스")],
+                  time: .점심),
+            .init(ingredients:
+                    [.init(isNew: true, name: "땅콩"), .init(isNew: false, name: "쌀"), .init(isNew: false, name: "소고기"), .init(isNew: false, name: "사과"), .init(isNew: false, name: "단호박")],
+                  time: .점심),
+        ]
+        let meal26: [MealPlan.Meal] = [
+            .init(ingredients:
+                    [.init(isNew: false, name: "쌀"), .init(isNew: false, name: "소고기"), .init(isNew: false, name: "사과"), .init(isNew: false, name: "브로컬리")],
+                  time: .아침),
+            .init(ingredients:
+                    [.init(isNew: true, name: "아스파라거스"), .init(isNew: false, name: "쌀"), .init(isNew: false, name: "소고기"), .init(isNew: false, name: "단호박")],
+                  time: .저녁)
+        ]
+        return [
+            // MockData 용.. 강제 언래핑이니까 봐줘..
+            MealPlan(startDate: .date(year: 2023, month: 10, day: 14)!, endDate: .date(year: 2023, month: 10, day: 16)!, meals: meal14, solidFoodDays: 13),
+            MealPlan(startDate: .date(year: 2023, month: 10, day: 17)!, endDate: .date(year: 2023, month: 10, day: 17+2)!, meals: meal17, solidFoodDays: 16),
+            MealPlan(startDate: .date(year: 2023, month: 10, day: 20)!, endDate: .date(year: 2023, month: 10, day: 20+2)!, meals: meal20, solidFoodDays: 19),
+            MealPlan(startDate: .date(year: 2023, month: 10, day: 23)!, endDate: .date(year: 2023, month: 10, day: 23+2)!, meals: meal23, solidFoodDays: 22),
+            MealPlan(startDate: .date(year: 2023, month: 10, day: 26)!, endDate: .date(year: 2023, month: 10, day: 26+2)!, meals: meal26, solidFoodDays: 25)
+        ]
+    }
+
     struct MealPlan: Identifiable {
         var id: UUID = UUID()
         var startDate: Date
