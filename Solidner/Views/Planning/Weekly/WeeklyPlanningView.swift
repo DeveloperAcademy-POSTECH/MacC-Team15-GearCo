cbba3f99d90003b1ca16a9bd63f19f2a4b7f7fb8
sei <vanism7557@gmail.com>
2023-11-10 03:06:40 +0900

    [Chore/#4]: Mock data를 위한 Date Extension 추가 및 활용
    - MealPlan의 solidFoodDaysString computed component
      - "n ~ n 일차 >" 를 계산하는 property

--- Solidner/Views/Planning/Weekly/WeeklyPlanningView.swift
+++ Solidner/Views/Planning/Weekly/WeeklyPlanningView.swift
@@ -23,9 +23,74 @@ struct WeeklyPlanningView: View {
                 print("편집 버튼 눌림")
             }
         }
+}
+
+
+extension Date {
+    static func date(year: Int, month: Int, day: Int) -> Date? {
+        var dateComponents = DateComponents()
+        dateComponents.year = year
+        dateComponents.month = month
+        dateComponents.day = day
+
+        return Calendar.current.date(from: dateComponents)
     }
 
+    static func - (lhs: Date, rhs: Int) -> Date {
+        return Calendar.current.date(byAdding: .day, value: -rhs, to: lhs)!
+    }
 
+    static func componentsBetweenDates(from startDate: Date, to endDate: Date) -> DateComponents {
+        Calendar.current.dateComponents([.day, .hour, .minute], from: startDate, to: endDate)
+    }
+
+    static func range(from startDate: Date, to endDate: Date) -> [Date] {
+        let diff = componentsBetweenDates(from: startDate, to: endDate).day!
+        return (0...diff).compactMap {
+            Calendar.current.date(byAdding: .day, value: $0, to: startDate)
+        }
+    }
+
+    var day: Int {
+        return Calendar.current.component(.day, from: self)
+    }
+
+    var weekday: Int {
+        return Calendar.current.component(.weekday, from: self)
+    }
+
+    var weekDayKor: String {
+        Weekday(rawValue: weekday)!.description
+    }
+
+    enum Weekday: Int {
+        case sunday = 1
+        case monday = 2
+        case tuesday = 3
+        case wednesday = 4
+        case thursday = 5
+        case friday = 6
+        case saturday = 7
+
+        var description: String {
+            switch self {
+            case .sunday:
+                return "일"
+            case .monday:
+                return "월"
+            case .tuesday:
+                return "화"
+            case .wednesday:
+                return "수"
+            case .thursday:
+                return "목"
+            case .friday:
+                return "금"
+            case .saturday:
+                return "토"
+            }
+        }
+    }
 }
 
 extension WeeklyPlanningView {
@@ -37,6 +102,13 @@ extension WeeklyPlanningView {
         var endDate: Date
         var meals: [Meal]
         var solidFoodDays: Int
+
+        var solidFoodDaysString: String {
+            TextLiterals.WeeklyPlanning.solidFoodDayText(
+                from: solidFoodDays,
+                to: solidFoodDays + (Date.componentsBetweenDates(from: startDate, to: endDate).day!)
+            )
+        }
 
         struct Meal: Identifiable, Hashable {
             var id: UUID = UUID()
