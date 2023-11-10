81c8beda6e2d44608feee05f5af2fa8fa1ba4a4f
220v-K <jwlee010222@gmail.com>
2023-11-11 05:48:45 +0900

    [Add, Remove/#8]: remove dummy file, add monthlyPlanningView file.

---
+++ Solidner/Views/Planning/Monthly/MonthlyPlanningView.swift
@@ -0,0 +1,20 @@
+//
+//  MonthlyPlanningView.swift
+//  Solidner
+//
+//  Created by 이재원 on 2023/11/13.
+//
+
+import SwiftUI
+
+struct MonthlyPlanningView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MonthlyPlanningView_Previews: PreviewProvider {
+    static var previews: some View {
+        MonthlyPlanningView()
+    }
+}
