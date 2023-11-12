6858f13032db5b8270f31298cb3dbedd661c7c81
sei <vanism7557@gmail.com>
2023-11-13 00:01:20 +0900

    [Add/#9] Add Plan View 파일 생성

---
+++ Solidner/Views/Planning/AddPlan/AddPlanView.swift
@@ -0,0 +1,18 @@
+//
+//  AddPlanView.swift
+//  Solidner
+//
+//  Created by sei on 11/13/23.
+//
+
+import SwiftUI
+
+struct AddPlanView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddPlanView()
+}
