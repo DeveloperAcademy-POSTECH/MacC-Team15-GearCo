c80b7cddd7b6c546c302f5a944a89c6692ed1b20
220v-K <jwlee010222@gmail.com>
2023-11-13 01:00:16 +0900

    [Feat/#8]: 월간 이유식 화면 캘린더 위 HStack(SubHeader) 작성

--- Solidner/Views/Planning/Monthly/MonthlyPlanningView.swift
+++ Solidner/Views/Planning/Monthly/MonthlyPlanningView.swift
@@ -9,7 +9,33 @@ import SwiftUI
 
 struct MonthlyPlanningView: View {
     var body: some View {
+        VStack(spacing: 0) {
+            // MARK: 임시로 대충 헤더 자리 비워두기
+            Spacer().frame(height: 60)
+            subHeader
+        }.padding(.horizontal, 14)
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+    
+    private var subHeader: some View {
+        HStack {
+            Text("월간 이유식").font(.title).bold()
+            Text("생후 +180").font(.system(size: 15, weight: .bold))
+                .foregroundColor(.gray)
+                .padding(.horizontal, 4)
+                .padding(.vertical, 3)
+                .background {
+                    RoundedRectangle(cornerRadius: 4)
+                        .foregroundColor(.gray)
+                        .opacity(0.3)
+                }
+            Spacer()
+            Text("전체").foregroundColor(.gray)
+            Image(systemName: "gearshape")
+                .font(.headline)
+                .bold()
+                .foregroundColor(.gray)
+        }
     }
 }
 
