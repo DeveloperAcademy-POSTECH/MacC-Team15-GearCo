367e9b0e34bd63a8b50c59fdcd7634236efc74da
earlysummer0303 <jw9914@naver.com>
2023-11-12 17:11:48 +0900

    Feat:TextFieldComponents 파일 구성 완료.

---
+++ Solidner/Views/Components/TextFieldComponents.swift
@@ -0,0 +1,36 @@
+//
+//  TextFieldComponents.swift
+//  Solidner
+//
+//  Created by 황지우2 on 2023/11/12.
+//
+
+import SwiftUI
+
+struct TextFieldComponents: View {
+    let placeHolder = "최대 10자내로  입력이 가능해요."
+    let textFieldColor = Color(#colorLiteral(red: 0.9277959466, green: 0.9277958274, blue: 0.9277958274, alpha: 1))
+    let placeHolderColor = Color(#colorLiteral(red: 0.6429678202, green: 0.6429678202, blue: 0.6429678202, alpha: 1))
+    let textFieldCornerRadius: CGFloat = 12
+    let textFieldPadding: CGFloat = 16
+    @State var value = ""
+    @FocusState var isFocused: Bool
+    
+    var body: some View {
+        shortTextfield(placeHolder: placeHolder, value: $value, isFocused: $isFocused)
+    }
+    
+    func shortTextfield(placeHolder: String, value: Binding<String>, isFocused: FocusState<Bool>.Binding) -> some View {
+        TextField("", text: value, prompt: Text(placeHolder).foregroundColor(placeHolderColor))
+            .focused(isFocused)
+            .padding(textFieldPadding)
+            .background(textFieldColor)
+            .cornerRadius(textFieldCornerRadius)
+    }
+}
+
+struct TextFieldComponents_Previews: PreviewProvider {
+    static var previews: some View {
+        TextFieldComponents()
+    }
+}
