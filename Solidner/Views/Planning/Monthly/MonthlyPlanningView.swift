5f2a7925ec13ea3fada6ce1291e958bfef825657
220v-K <jwlee010222@gmail.com>
2023-11-13 17:52:00 +0900

    [Feat/#8]: 월간 계획 뷰 틀 잡기

--- Solidner/Views/Planning/Monthly/MonthlyPlanningView.swift
+++ Solidner/Views/Planning/Monthly/MonthlyPlanningView.swift
@@ -8,38 +8,56 @@
 import SwiftUI
 
 struct MonthlyPlanningView: View {
+    let screenWidth = UIScreen.main.bounds.size.width
+    let screenHeight = UIScreen.main.bounds.size.height
+    
     let lightGray = Color(#colorLiteral(red: 0.8797428608, green: 0.8797428012, blue: 0.8797428608, alpha: 1)) // #D9D9D9
+    let weekDayKorList = ["일", "월", "화", "수", "목", "금", "토"]
     
     var body: some View {
         VStack(spacing: 0) {
             // MARK: 임시로 대충 헤더 자리 비워두기
             Spacer().frame(height: 70)
-            subHeader
+            calendarCurrentYearMonth.padding(.bottom, 15)
             
-            HStack(spacing: 0) {
-                
+            VStack(spacing: 0) {
+                calendarWeekDayRow
+                Spacer()
+            }.background {
+                RoundedRectangle(cornerRadius: 12)
+                    .foregroundColor(.white)
             }
             Spacer()
         }.padding(.horizontal, 14)
+            .background(Color(.lightGray))
     }
     
-    private var subHeader: some View {
-        HStack {
-            Text("월간 이유식").font(.title).bold()
-            Text("생후 +180").font(.system(size: 15, weight: .bold))
-                .foregroundColor(.gray)
-                .padding(.horizontal, 4)
-                .padding(.vertical, 3)
-                .background {
-                    RoundedRectangle(cornerRadius: 4)
-                        .foregroundColor(lightGray)
+    private var calendarWeekDayRow: some View {
+        let mainHorizontalPadding = screenWidth * (6/390)
+        let mainDaySectionWidth = screenWidth * (50/390)
+        
+        return HStack(alignment: .center, spacing: 0) {
+            ForEach(weekDayKorList.indices) { i in
+                if i == 0 {
+                    Text(weekDayKorList[i])
+                        .frame(width: mainDaySectionWidth)
+                        .padding(.leading, mainHorizontalPadding)
+                } else if i == 6 {
+                    Text(weekDayKorList[i])
+                        .frame(width: mainDaySectionWidth)
+                        .padding(.trailing, mainHorizontalPadding)
+                } else {
+                    Text(weekDayKorList[i])
+                        .frame(width: mainDaySectionWidth)
                 }
+            }
+        }.frame(height: 25)
+    }
+    
+    private var calendarCurrentYearMonth: some View {
+        HStack {
+            Text("2023년 10월").font(.title).bold()
             Spacer()
-            Text("전체").foregroundColor(.gray)
-            Image(systemName: "gearshape")
-                .font(.headline)
-                .bold()
-                .foregroundColor(.gray)
         }
     }
 }
