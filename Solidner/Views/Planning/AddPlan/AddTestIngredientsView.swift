66f8bd815265ac6939e8107d3c424c0697b6a37d
220v-K <jwlee010222@gmail.com>
2023-11-21 15:55:58 +0900

    [Update, Refactor/#18]: backButtonHeader 컴포넌트 수정 및 타이틀 추가

---
+++ Solidner/Views/Planning/AddPlan/AddTestIngredientsView.swift
@@ -0,0 +1,22 @@
+//
+//  AddTestIngredientsView.swift
+//  Solidner
+//
+//  Created by 이재원 on 2023/11/21.
+//
+
+import SwiftUI
+
+struct AddTestIngredientsView: View {
+    var body: some View {
+        VStack(spacing: 0) {
+            BackButtonHeader()
+        }
+    }
+}
+
+struct AddTestIngredientsView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddTestIngredientsView()
+    }
+}
