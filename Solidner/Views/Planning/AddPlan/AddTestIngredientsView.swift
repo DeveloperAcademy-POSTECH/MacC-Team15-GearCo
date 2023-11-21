c1036ce23b01632efd5081d6ca6b8ead1f813995
220v-K <jwlee010222@gmail.com>
2023-11-21 17:45:51 +0900

    [Feat/#18]: AddIngredientView 상단 분류 버튼

--- Solidner/Views/Planning/AddPlan/AddTestIngredientsView.swift
+++ Solidner/Views/Planning/AddPlan/AddTestIngredientsView.swift
@@ -13,14 +13,86 @@ struct AddTestIngredientsView: View {
     var body: some View {
         VStack(spacing: 0) {
             BackButtonHeader(title: Texts.testViewTitle)
-            Spacer()
+                .padding(.bottom, 20)
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: 10) {
+                    searchButton
+                    ForEach(TempIngredientType.allCases, id: \.self) { ingredientType in
+                        ingredientTypeButton(ingredientType.rawValue)
+                    }
+                }.padding(.leading, 20)
+            }
             
             ScrollView {
                 VStack(spacing: 0) {
-                    
+                    Spacer()
                 }
             }
         }.background(Color.mainBackgroundColor)
+    }
+    
+    private func ingredientTypeButton(_ text: String) -> some View {
+        let textHorizontalPadding: CGFloat = 17
+        let buttonFrameHeight: CGFloat = 40
+        let buttonBackgroundRadius: CGFloat = 27.5
+        
+        return Button {
+            // TODO: Change State of Button
+        } label: {
+            // TODO: Color Change, font setting
+            Text(text)
+                .foregroundColor(.white)
+                .font(.system(size: 15, weight: .medium))
+                .padding(.horizontal, textHorizontalPadding)
+                .frame(height: buttonFrameHeight)
+                .background {
+                    RoundedRectangle(cornerRadius: buttonBackgroundRadius)
+                        .fill(Color.black)
+                }
+        }
+    }
+    
+    private var searchButton: some View {
+        let searchIconSize: CGFloat = 20
+        let searchIconHorizontalPadding: CGFloat = 25
+        let buttonFrameHeight: CGFloat = 40
+        let searchButtonBackgroundRadius: CGFloat = 24
+        let searchButtonStrokeInset: CGFloat = 0.5
+        let searchButtonStrokeLineWidth: CGFloat = 1
+        
+        return Button {
+            // TODO: Search
+        } label: {
+            // TODO: Color Change, font setting
+            Image(systemName: "magnifyingglass")
+                .resizable()
+                .scaledToFit()
+                .frame(width: searchIconSize)
+                .foregroundColor(.gray)
+                .padding(.horizontal, searchIconHorizontalPadding)
+                .frame(height: buttonFrameHeight)
+
+                .background {
+                    RoundedRectangle(cornerRadius: searchButtonBackgroundRadius)
+                        .fill(Color.mainBackgroundColor)
+                }.overlay {
+                    RoundedRectangle(cornerRadius: searchButtonBackgroundRadius)
+                        .inset(by: searchButtonStrokeInset)
+                        .stroke(.black.opacity(0.1), lineWidth: searchButtonStrokeLineWidth)
+                }
+        }
+    }
+    
+    // MARK: IngredientType
+    enum TempIngredientType: String, CaseIterable {
+        case 이상반응재료 = "이상 반응 재료"
+        case 곡류 = "곡류"
+        case 채소류 = "채소류"
+        case 과일류 = "과일류"
+        case 육어류 = "육류&어류"
+        case 유제품류 = "유제품류"
+        case 기타 = "기타"
     }
 }
 
