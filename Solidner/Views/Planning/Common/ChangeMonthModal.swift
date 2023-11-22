278a58202102deff3efacd4bb445f4863903dcee
sei <vanism7557@gmail.com>
2023-11-22 15:20:22 +0900

    [Add/#28]: ChangeMonthModal 파일 추가

---
+++ Solidner/Views/Planning/Common/ChangeMonthModal.swift
@@ -0,0 +1,18 @@
+//
+//  ChangeMonthModal.swift
+//  Solidner
+//
+//  Created by sei on 11/22/23.
+//
+
+import SwiftUI
+
+struct ChangeMonthModal: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChangeMonthModal()
+}
