132be8cc42597247f132f53a19ec302019b579f2
sei <vanism7557@gmail.com>
2023-11-22 15:34:15 +0900

    [Feat/#28] monthTitleBar 구현
    - K 에 magic string 집어넣기
    - enum chevron 자체는 다른 뷰에도 쓰일 수 있으면 바깥으로 빼도 무방할듯

--- Solidner/Views/Planning/Common/ChangeMonthModal.swift
+++ Solidner/Views/Planning/Common/ChangeMonthModal.swift
@@ -8,11 +8,83 @@
 import SwiftUI
 
 struct ChangeMonthModal: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    @Binding var selectedDate: Date
+    @State private var selectingDate: Date
+    
+    private enum K {
+        static var leftButtonIcon: String { "chevron.left" }
+        static var rightButtonIcon: String { "chevron.right" }
+        static var monthTitleColor: Color { Color.black }
     }
+    private let texts = TextLiterals.ChangeMonth.self
+
+    init(selectedDate: Binding<Date>) {
+        self._selectedDate = selectedDate
+        self._selectingDate = State(initialValue: selectedDate.wrappedValue)
+    }
+
+    var body: some View {
+        VStack {
+            monthTitleBar
+        }
+    }
+
+    private var monthTitleBar: some View {
+        HStack {
+            leftChevron
+            Spacer()
+            monthTitle
+            Spacer()
+            rightChevron
+        }
+    }
+
+    private var leftChevron: some View {
+        chevronView(direction: .left) {
+            selectingDate = selectingDate.add(.year, value: -1)
+        }
+    }
+
+    private var rightChevron: some View {
+        chevronView(direction: .right) {
+            selectingDate = selectingDate.add(.year, value: 1)
+        }
+    }
+
+    enum Chevron {
+        case left, right
+
+        var iconImage: some View {
+            switch self {
+            case .left:
+                return Image(systemName: K.leftButtonIcon)
+            case .right:
+                return Image(systemName: K.rightButtonIcon)
+            }
+        }
+    }
+
+    private func chevronView(direction chevron: Chevron, action: @escaping ()->Void) -> some View {
+        Button {
+            withAnimation {
+                action()
+            }
+        } label: {
+            chevron.iconImage
+        }
+        .foregroundStyle(K.chevronColor)
+    }
+
+    private var monthTitle: some View {
+        Text(texts.currentYearText(of: selectingDate))
+            .font(.title)
+            .foregroundStyle(K.monthTitleColor)
+    }
+
 }
 
-#Preview {
-    ChangeMonthModal()
+struct ChangeMonthModal_Previews: PreviewProvider {
+    static var previews: some View {
+        ChangeMonthModal(selectedDate: .constant(Date()))
+    }
 }
