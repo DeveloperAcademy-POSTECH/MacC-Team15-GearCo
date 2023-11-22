bb13a0c9b2f5f957a8084e23c267c43cdcaa7dcf
sei <vanism7557@gmail.com>
2023-11-22 15:44:34 +0900

    [Add/#31]: PlanBatchSettingView 파일 추가

---
+++ Solidner/Views/Planning/BatchSetting/PlanBatchSettingView.swift
@@ -0,0 +1,20 @@
+//
+//  PlanBatchSettingView.swift
+//  Solidner
+//
+//  Created by sei on 11/22/23.
+//
+
+import SwiftUI
+
+struct PlanBatchSettingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PlanBatchSettingView_Previews: PreviewProvider {
+    static var previews: some View {
+        PlanBatchSettingView()
+    }
+}
