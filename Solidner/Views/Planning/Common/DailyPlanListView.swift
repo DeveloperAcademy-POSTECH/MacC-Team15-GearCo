e873c664b0397e20573cec4c420fd831a22161f9
sei <vanism7557@gmail.com>
2023-11-22 18:48:30 +0900

    [Add/#33]: DailyPlanListView 파일 생성

---
+++ Solidner/Views/Planning/Common/DailyPlanListView.swift
@@ -0,0 +1,23 @@
+//
+//  DailyPlanListView.swift
+//  Solidner
+//
+//  Created by sei on 11/22/23.
+//
+
+import SwiftUI
+
+struct DailyPlanListView: View {
+    var body: some View {
+        VStack {
+            Text("Hello world")
+        }
+    }
+}
+
+
+struct DailyPlanListView_Previews: PreviewProvider {
+    static var previews: some View {
+        DailyPlanListView()
+    }
+}
