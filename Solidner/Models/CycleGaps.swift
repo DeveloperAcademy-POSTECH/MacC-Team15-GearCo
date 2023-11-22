eb55040cdb4a6c612469cef9d643df3ab17e89e9
sei <vanism7557@gmail.com>
2023-11-22 18:24:39 +0900

    [Add/#31]: CycleGap, DisplayDateType 모델 파일 생성
    - CycleGap: 플랜 간격
    - DisplayDataType: 주간, 월간의 날짜 표시 방식

---
+++ Solidner/Models/CycleGaps.swift
@@ -0,0 +1,8 @@
+//
+//  Cycles.swift
+//  Solidner
+//
+//  Created by sei on 11/22/23.
+//
+
+import Foundation
---
+++ Solidner/Models/DisplayDateType.swift
@@ -0,0 +1,8 @@
+//
+//  DisplayDateType.swift
+//  Solidner
+//
+//  Created by sei on 11/22/23.
+//
+
+import Foundation
