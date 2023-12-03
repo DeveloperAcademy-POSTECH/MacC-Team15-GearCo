4c03afd2c11f05bcf4a706342d43abcf5494eab0
sei <vanism7557@gmail.com>
2023-12-03 22:35:02 +0900

    [Design/#110]: 버튼 글귀 저장 -> 이 달의 계획 보기

--- Solidner/Literal/TextLiterals.swift
+++ Solidner/Literal/TextLiterals.swift
@@ -89,7 +89,7 @@ enum TextLiterals {
     enum ChangeMonth {
         static func currentYearText(of year: Int) -> String { "\(year)년" }
         static func monthText(of number: Int) -> String { "\(number)월" }
-        static var saveButtonText: String { "저장" }
+        static var saveButtonText: String { "이 달의 계획 보기" }
     }
 
     enum Warning {
